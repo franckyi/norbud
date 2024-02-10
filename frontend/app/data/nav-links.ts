ebfae9a7bd330819e940962287438640577d3ed3
Francky <contact@francky.pl>
2024-02-10 19:12:45 +0100

    temporarily disable career view

--- frontend/app/data/nav-links.ts
+++ frontend/app/data/nav-links.ts
@@ -2,5 +2,5 @@ export const NAV_LINKS = [
   { href: "/#about-us", label: "O firmie" },
   { href: "/realizacje", label: "Realizacje" },
   { href: "/wynajem", label: "Wynajem" },
-  { href: "/kariera", label: "Kariera" },
+  // { href: "/kariera", label: "Kariera" },
 ];
