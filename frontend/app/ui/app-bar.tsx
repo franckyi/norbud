6f124df8ee57cda2b2151a091483005d0d757576
[Tiffania] <ielenatiffania@gmail.com>
2024-02-13 16:59:44 -0500

    Animation for border bottom

--- frontend/app/ui/app-bar.tsx
+++ frontend/app/ui/app-bar.tsx
@@ -1,9 +1,13 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
+
 import { NAV_LINKS } from "../data/nav-links";
 
 export default function AppBar() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
     <div className="flex justify-center gap-8 top-0 lg:top-8 z-50 lg:pt-8 lg:ml-80">
       <Image
@@ -17,14 +21,24 @@ export default function AppBar() {
       <nav
         className="flex items-center gap-16 p-3 lg:px-4 lg:pl-12 lg:w-full text-sm tracking-wide lg:font-semibold leading-6 bg-green-700 text-neutral-100 uppercase"
         aria-label="Global"
+        style={{ height: "100%" }}
       >
-        {NAV_LINKS.map(({ href, label }) => (
-          <Link
-            key={label}
-            href={href}
-            className="border-solid border-b-4 border-green-700 hover:border-red-700"
-          >
-            {label}
+        {NAV_LINKS.map(({ href, label }, index) => (
+          <Link key={label} href={href}>
+            <div
+              className="relative"
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <div
+                className="absolute inset-x-0 bottom-0 border-b-4 border-transparent transition-all duration-300"
+                style={{
+                  width: hoveredIndex === index ? "100%" : "0",
+                  borderColor: hoveredIndex === index ? "#E53E3E" : "#48BB78",
+                }}
+              />
+              <div className="relative z-10">{label}</div>
+            </div>
           </Link>
         ))}
         <a
