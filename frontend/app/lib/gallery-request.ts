0f5694150e1f16020360fc201c418423b5a02ce2
Francky <contact@francky.pl>
2024-02-22 10:46:25 +0100

    retrieve more posts

--- frontend/app/lib/gallery-request.ts
+++ frontend/app/lib/gallery-request.ts
@@ -1,6 +1,7 @@
 const DOMAIN_NAME = "https://admin.nor-bud.com";
-const WPPATH = `wp-json/wp/v2/norbud_gallery`;
+const WPPATH = "wp-json/wp/v2/norbud_gallery";
+const PER_PAGE = 100;
 
 export const galleryRequest = {
-  URL: `${DOMAIN_NAME}/${WPPATH}`,
+  URL: `${DOMAIN_NAME}/${WPPATH}?per_page=${PER_PAGE}`,
 };
