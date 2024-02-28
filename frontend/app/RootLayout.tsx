fa6093d064fc8c04e4ddfaf528a45fa72364ec83
Francky <contact@francky.pl>
2024-02-28 20:18:37 +0100

    update layout

--- frontend/app/RootLayout.tsx
+++ frontend/app/RootLayout.tsx
@@ -1,16 +1,39 @@
-import AppBar from "./ui/common/app-bar/app-bar";
+"use client";
+import "./globals.css";
+import Hero from "./ui/common/hero";
 import { archivo } from "./ui/common/fonts";
+import Footer from "./ui/common/footer";
+import AppBar from "./ui/common/app-bar/app-bar";
+import { ReactNode, useState, createContext } from "react";
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+const bodyClasses =
+  "bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-400";
+
+const ThemeContext = createContext<null | string>(null);
+
+function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  const [theme, setTheme] = useState("dark");
+
+  function toggleTheme() {
+    if (theme === "dark") {
+      setTheme("");
+    } else {
+      setTheme("dark");
+    }
+  }
+
   return (
-    <html lang="pl">
-      <body className={archivo.className}>
-        <AppBar />
-        <main>{children}</main>
-        <footer>@ 2024 Norbud. All rights reserved.</footer>
-      </body>
-    </html>
+    <ThemeContext.Provider value={theme}>
+      <html lang="pl" className={`scroll-smooth ${theme}`}>
+        <body className={`${archivo.className} ${bodyClasses}`}>
+          <AppBar toggleTheme={toggleTheme} />
+          <Hero />
+          <main className="text-center">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ThemeContext.Provider>
   );
 }
+
+export default RootLayout;
