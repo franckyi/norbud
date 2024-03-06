aa980d1e07b00d2b2d8fc8df598a5b7b619c6963
Francky <contact@francky.pl>
2024-03-06 18:18:56 +0100

    replace email with form

--- frontend/app/ui/common/write-us-button.tsx
+++ frontend/app/ui/common/write-us-button.tsx
@@ -1,15 +1,16 @@
 import { WriteUsButtonProps } from "@/app/types/write-us-button-props";
+import Link from "next/link";
 
 const defaultClasses =
   "inline-block max-w-lg px-4 py-1 rounded-full text-center";
 function WriteUsButton({ bg, bgHover, textColor }: WriteUsButtonProps) {
   return (
-    <a
-      href="mailto:biuro@nor-bud.com"
+    <Link
+      href="/#contact-form"
       className={`${defaultClasses} ${bg} ${bgHover} ${textColor}`}
     >
       Zleć projekt
-    </a>
+    </Link>
   );
 }
 
