a0683ab06ba246ab02a36a773f75e21d2c7826a1
Francky <contact@francky.pl>
2024-03-06 17:17:39 +0100

    fix export error

--- frontend/app/ui/contact/contact-section.tsx
+++ frontend/app/ui/contact/contact-section.tsx
@@ -1,8 +1,5 @@
 import { CompanyInfo } from "@/app/types/company-info";
-import { ContactSection } from "./ContactSection";
 
 export interface ContactSectionProps {
   companyInfo: CompanyInfo;
 }
-
-export default ContactSection;
