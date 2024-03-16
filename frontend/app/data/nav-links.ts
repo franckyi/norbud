82d9a4d4c9534ade46a00c676034bd967ff4ea46
Francky <contact@francky.pl>
2024-03-16 22:06:02 +0100

    add career page link

--- frontend/app/data/nav-links.ts
+++ frontend/app/data/nav-links.ts
@@ -2,4 +2,5 @@ export const NAV_LINKS = [
   { id: 1, href: "/#ofirmie", label: "O firmie" },
   { id: 2, href: "/realizacje", label: "Realizacje" },
   { id: 3, href: "/wynajem", label: "Wynajem" },
+  { id: 4, href: "/kariera", label: "Kariera" },
 ];
