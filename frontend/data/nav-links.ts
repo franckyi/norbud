f9241a4a2c397e5ed5555bc322ac626af6fd47f1
Francky <contact@francky.pl>
2024-03-25 21:49:41 +0100

    add link

--- frontend/data/nav-links.ts
+++ frontend/data/nav-links.ts
@@ -2,5 +2,6 @@ export const NAV_LINKS = [
   { id: 1, href: "/#ofirmie", label: "O firmie" },
   { id: 2, href: "/realizacje", label: "Realizacje" },
   { id: 3, href: "/wynajem", label: "Wynajem" },
+  { id: 3, href: "https://sklep.nor-bud.com", label: "Sklep" },
   { id: 4, href: "/kariera", label: "Kariera" },
 ];
